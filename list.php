3ae5df8397c1aecb263e48cb88519e6619cf5567
comingforyou21 <haianh6101991@gmail.com>
2021-07-02 11:48:41 +0700

    Initial commit

---
+++ list.php
@@ -0,0 +1,15 @@
+<?php 
+if ($handle = opendir('./a')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
+
+ ?>
